a5d9fcbd2b2475f6463356165be8abccd2d07d3b
Luke Bridges <luke.bridges6@studytafensw.edu.au>
2024-09-20 16:06:36 +1000

    Guns guns guns

--- Assessment 2 Game Design/Assets/Scripts/PlayerController.cs
+++ Assessment 2 Game Design/Assets/Scripts/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ammoDisplay.text = bulletInGun.ToString("Ammo " + bulletInGun);
+        ammoDisplay.text = bulletInGun.ToString("Ammo " + bulletInGun.ToString());
 
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
 
